14a44ef673c335677bc9bab10d82598c3b24cbd9
Fanha Giang <fanha99@hotmail.com>
2016-12-20 20:32:02 +0700

    CallerWindow tyoe while compiler with api level 19+

--- app/src/main/java/com/odoo/addons/phonecall/features/CallerWindow.java
+++ app/src/main/java/com/odoo/addons/phonecall/features/CallerWindow.java
@@ -58,12 +58,12 @@ public class CallerWindow {
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.MATCH_PARENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                WindowManager.LayoutParams.TYPE_TOAST,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
                         | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
                         | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
                 PixelFormat.TRANSLUCENT);
-		// Fanha: not always on top, because some phone
+        // Fanha: not always on top, because some phone
         // params.gravity = Gravity.TOP;
         return params;
     }
@@ -119,12 +119,12 @@ public class CallerWindow {
                     callerView = buildView();
                     bindView(row);
                     WindowManager.LayoutParams params = getWindowParams();
-					// Fanha: why need to be the bottom?
+                    // Fanha: why need to be the bottom?
                     // if (!dialed && !inIdleMode() && isLollipop()) {
                        // params.gravity = Gravity.BOTTOM;
                     // }
                     windowManager.addView(callerView, params);
-					// Fanha: click to dimiss, sometime it is not dimiss while end call
+                    // Fanha: click to dimiss, sometime it is not dimiss while end call
                     callerView.setOnClickListener(new View.OnClickListener() {
 
                         @Override
